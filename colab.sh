0df4455f729fd38971569bc83139cb86ffd751ae
Stephen <champs4@hotmail.com>
2020-07-27 19:08:41 +0800

    added colab.sh

--- colab.sh
+++ colab.sh
@@ -4,6 +4,10 @@ pip install -q omegaconf
 pip install -q --upgrade --force-reinstall --no-deps kaggle
 git clone https://github.com/stephenllh/wheat_efficientdet.git
 
+python -c '
+import os; os.environ["KAGGLE_CONFIG_DIR"] = "drive/My Drive/.kaggle"
+'
+
 unzip -q drive/My Drive/DL/kaggle/global_wheat_detection/data.zip -d wheat_efficientdet/input
 mkdir wheat_efficientdet/pretrained_models
 unzip -q efficientdet.zip -d wheat_efficientdet/pretrained_models
@@ -11,6 +15,6 @@ unzip -q efficientdet.zip -d wheat_efficientdet/pretrained_models
 cd wheat_efficientdet/src
 
 python train.py \
---epoch=10 \
+--epoch=100 \
 --model-variant=d0 \
 --bs=8 \
